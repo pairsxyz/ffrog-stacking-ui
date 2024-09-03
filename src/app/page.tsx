481d6b7f08cf8d0413c826292b10c4a16dacf1b0
maha-saaa <mahsaa.badiee@gmail.com>
2024-09-03 17:04:12 +0300

    Add mobile version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,12 @@
 import Image from "next/image";
 import { useState } from "react";
 import background from "../../public/background.webp";
-import bush1 from "../../public/bush-1.webp";
 import bush2 from "../../public/bush-2.webp";
 import bush3 from "../../public/bush-3.webp";
 import drFrog from "../../public/dr-frog.webp";
 import frog from "../../public/frog.webp";
 import ground from "../../public/ground.webp";
 import leef from "../../public/leef.webp";
-import rock from "../../public/rock.webp";
 import mainText from "../../public/main-text.webp";
 import frog2 from "../../public/frog-2.webp";
 import poolText from "../../public/pool-text.webp";
@@ -34,24 +32,61 @@ export default function Home() {
         sizes="100vw"
       />
 
-      <div className="absolute left-[30%]">
+      <div className="w-full flex flex-col gap-2 items-center justify-center">
         <Image
-          className="w-[484px] h-[84px] object-contain"
+          className="w-[349px] h-[61px] xl:w-[484px] xl:h-[84px] object-contain"
           src={mainText}
           alt="background"
           priority
           sizes="100vw"
         />
 
-        <p className="text-3xl font-medium text-[#005B0F]">POOL SIZE</p>
-        <p className="text-base font-medium text-white text-start">
-          Stake your $FFROG to participate in profit sharing from the
-          <br /> entire $FFROG ecosystem. Rewards are issued daily.
+        <div className="w-full xl:w-[45%] flex flex-col items-center xl:items-start justify-center">
+          <p className="text-3xl font-medium text-[#005B0F] text-center xl:text-start">
+            POOL SIZE
+          </p>
+          <p className="text-[8px] xl:text-base font-medium text-white text-center xl:text-start">
+            Stake your $FFROG to participate in profit sharing from the
+            <br /> entire $FFROG ecosystem. Rewards are issued daily.
+          </p>
+        </div>
+      </div>
+
+      <div
+        className="w-[340px] h-[190px] xl:w-[473px] xl:h-[256px] mt-[25%] md:mt-10 flex flex-col items-center justify-center text-center"
+        style={{
+          backgroundImage: "url(/bush-1.webp)",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <Image
+          className="w-[139px] h-[49px] xl:w-[213px] xl:h-[75px] object-contain z-10"
+          src={poolText}
+          alt="background"
+          priority
+          sizes="100vw"
+        />
+        <p className="text-base xl:text-xl font-medium text-black z-10">
+          18% APY
         </p>
+        <p className="text-base font-medium text-black z-10">
+          Unlock Period of 7 days
+        </p>
+        <p className="w-1/2 xl:w-full text-[10px] font-medium text-[#005B0F]">
+          Once you unstake, a timer of 7 days will begin
+        </p>
+        <button
+          className="w-[167px] h-[35px] translate-y-16 sm:translate-y-10 xl:translate-y-0 rounded-[19px] border border-black z-10 bg-[#005B0F]"
+          style={{ boxShadow: "2.096px 2.795px 2.795px 0px #000" }}
+          onClick={() => setStakeModalIsOpen(true)}
+        >
+          <span className="text-[25px] font-normal text-white">STAKE</span>
+        </button>
       </div>
 
       <button
-        className="w-[278px] h-[129px] absolute right-20 top-0 flex items-center justify-center"
+        className="w-[200px] xl:w-[278px] h-[129px] hidden xl:flex items-center justify-center absolute right-20 top-0"
         onClick={() => setConnectWalletModalIsOpen(true)}
       >
         <Image
@@ -62,14 +97,14 @@ export default function Home() {
           sizes="100vw"
           fill
         />
-        <p className="text-xl font-medium text-[#005B0F] z-10">
+        <p className="text-base xl:text-xl font-medium text-[#005B0F] z-10">
           CONNECT
           <br /> WALLET
         </p>
       </button>
 
       <button
-        className="w-[250px] h-[159px] absolute right-20 top-[30%] flex items-center justify-center"
+        className="w-[150px] h-[96px] xl:w-[250px] xl:h-[159px] absolute right-[-10%] md:right-20 top-[20%] xl:top-[30%] flex items-center justify-center z-20 xl:z-0"
         onClick={() => setUnstakeModalIsOpen(true)}
       >
         <Image
@@ -80,78 +115,43 @@ export default function Home() {
           sizes="100vw"
           fill
         />
-        <p className="text-xl font-medium text-[#005B0F] mt-[10%] z-10">
+        <p className="text-xs xl:text-xl font-medium text-[#005B0F] mt-[10%] z-10">
           UNSTAKE
         </p>
       </button>
 
-      <div className="w-[473px] h-[256px] absolute left-[30%] top-[25%] flex flex-col items-center justify-center">
-        <Image
-          className="object-contain"
-          src={bush1}
+      <div className="w-[133px] h-[72px] xl:w-[344px] xl:h-[219px] absolute left-0 top-[22%] xl:top-[10%] flex items-center justify-center z-20">
+        {/* TODO: NO STAKE STATE */}
+        {/* <Image
+          className="w-[133px] h-[72px] object-contain"
+          src={bush2}
           alt="background"
           priority
           sizes="100vw"
           fill
         />
-        <Image
-          className="w-[213px] h-[75px] object-contain z-10"
-          src={poolText}
-          alt="background"
-          priority
-          sizes="100vw"
-        />
-        <p className="text-xl font-medium text-black z-10">18% APY</p>
-        <p className="text-base font-medium text-black z-10">
-          Unlock Period of 7 days
-        </p>
-        <p className="text-[10px] font-medium text-[#005B0F]">
-          Once you unstake, a timer of 7 days will begin
-        </p>
-        <button
-          className="w-[167px] h-[35px] rounded-[19px] border border-black z-10 bg-[#005B0F]"
-          style={{ boxShadow: "2.096px 2.795px 2.795px 0px #000" }}
-          onClick={() => setStakeModalIsOpen(true)}
-        >
-          <span className="text-[25px] font-normal text-white">STAKE</span>
-        </button>
+        <p className="text-xs xl:text-xl font-medium text-white ml-4 xl:ml-0 xl:mr-10 z-10">
+          NO $FFROG REWARDED
+        </p> */}
+
+        {/* TODO: FROG STAKED */}
+        {/* <p className="w-[80%] text-[8px] xl:text-[30px] font-medium text-white z-10">
+          CURRENT REWARDS 2,000 $FFROG
+        </p> */}
       </div>
 
-      <div className="w-[444px] h-[199px] absolute right-0 bottom-0 flex items-end justify-end">
-        <Image
-          className="object-contain"
-          src={ground}
-          alt="background"
-          priority
-          sizes="100vw"
-          fill
-        />
-        <p className="text-[25px] font-medium text-[#826914] z-10 mb-[5%] mr-[8%]">
-          COMING SOON...
-        </p>
-      </div>
-
-      <Image
-        className="w-[159px] h-[227px] object-contain absolute right-0 sm:right-[25%] bottom-[20%]"
-        src={drFrog}
-        alt="background"
-        priority
-        sizes="100vw"
-      />
-
-      <div className="w-[806px] h-[220px] absolute left-0 sm:left-[20%] bottom-8 flex flex-col items-center justify-center">
-        <Image
-          className="object-contain"
-          src={rock}
-          alt="background"
-          priority
-          sizes="100vw"
-          fill
-        />
-        <p className="text-3xl font-medium text-[#3D3D3D] mt-4 z-10">
+      <div
+        className="w-[777px] h-[212px] xl:w-[1019px] xl:h-[279px] flex flex-col items-center justify-center 4xl:translate-y-[180%]"
+        style={{
+          backgroundImage: "url(/rock.webp)",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
+      >
+        <p className="text-base xl:text-3xl font-medium text-[#3D3D3D] mt-4 z-10">
           MY STAKES & REWARDS
         </p>
-        <p className="text-2xl font-medium text-white z-10">
+        <p className="text-base xl:text-2xl font-medium text-white z-10">
           Connect Your Wallet
         </p>
         <button
@@ -166,42 +166,44 @@ export default function Home() {
 
         {/* TODO: NO STAKE STATE */}
         {/* <Image
-          className="w-[130px] h-[142px] object-contain z-10"
+          className="w-[86px] h-[94px] xl:w-[130px] xl:h-[142px] object-contain z-10"
           src={frog2}
           alt="background"
           priority
           sizes="100vw"
         />
-        <p className="text-[30px] font-medium text-white z-10">No Stakes Yet</p> */}
+        <p className="text-xl xl:text-[30px] font-medium text-white z-10">No Stakes Yet</p> */}
 
         {/* TODO: FROG STAKED */}
-        {/* <p className="text-[30px] font-medium text-white z-10">
+        {/* <p className="text-xl xl:text-[30px] font-medium text-white z-10">
           500,000 $FFROG Staked
         </p> */}
       </div>
 
-      <div className="w-[344px] h-[219px] absolute left-0 top-[10%] flex items-center justify-center">
-        {/* TODO: NO STAKE STATE */}
-        {/* <Image
-          className="object-contain"
-          src={bush2}
+      <div className="w-[283px] h-[111px] xl:w-[444px] xl:h-[199px] absolute right-0 bottom-0 flex items-end justify-end z-10">
+        <Image
+          className="object-cover"
+          src={ground}
           alt="background"
           priority
           sizes="100vw"
           fill
         />
-        <p className="text-xl font-medium text-white mr-10 z-10">
-          NO $FFROG REWARDED
-        </p> */}
-
-        {/* TODO: FROG STAKED */}
-        {/* <p className="text-[30px] font-medium text-white z-10">
-          CURRENT REWARDS 2,000 $FFROG
-        </p> */}
+        <p className="text-base xl:text-[25px] font-medium text-[#826914] z-10 mb-[5%] mr-[8%]">
+          COMING SOON...
+        </p>
       </div>
 
       <Image
-        className="w-[200px] lg:w-[338px] h-[374px] object-contain absolute left-0 bottom-0"
+        className="hidden xl:block w-[159px] h-[227px] object-contain absolute right-0 sm:right-[25%] bottom-[25%] -z-10"
+        src={drFrog}
+        alt="background"
+        priority
+        sizes="100vw"
+      />
+
+      <Image
+        className="hidden xl:block w-[200px] xl:w-[338px] h-[374px] object-contain absolute left-0 bottom-0"
         src={frog}
         alt="background"
         priority
